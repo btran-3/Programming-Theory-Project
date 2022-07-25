1354ad3c25ac7d12e18237bfa6d5ef07f7beae6e
bt <britra832@gmail.com>
2022-07-25 11:48:15 -0400

    URP, import 3d models

---
+++ Assets/Scripts/GameMaster.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMaster : MonoBehaviour
+{
+
+    //current health, pickups
+    //stat upgrades
+    //room that the player is in
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
