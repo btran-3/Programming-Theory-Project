7eaeafd2f68a719813135f15cd068cb676c8964e
bt <britra832@gmail.com>
2022-07-31 18:41:58 -0400

    Add doors and mood lighting to item room

--- Assets/Scripts/RoomWithItemsBehavior.cs
+++ Assets/Scripts/RoomWithItemsBehavior.cs
@@ -9,8 +9,17 @@ public class RoomWithItemsBehavior : MonoBehaviour
 
     [SerializeField] List<GameObject> spawnedUpgradeItems;
 
+    private Vector3 doorDefaultScale;
+    [SerializeField] GameObject doorTop;
     [SerializeField] GameObject doorBottom;
+    [Space(10)]
     [SerializeField] GameObject playerStartPos;
+    [Space(10)]
+
+    private float mainLightDefaultIntensity;
+    private float spotLightMaxIntensity;
+    [SerializeField] Light mainLight;
+    [SerializeField] List<Light> spotLights;
 
     private Collider colliderA;
 
@@ -31,10 +40,15 @@ public class RoomWithItemsBehavior : MonoBehaviour
 
     private void Start()
     {
+        doorBottom.SetActive(false);
         colliderA = GetComponent<Collider>();
         playerStartPos.GetComponent<Renderer>().enabled = false;
 
         InitializeUpgradeItems();
+
+        doorDefaultScale = doorTop.transform.localScale;
+        mainLightDefaultIntensity = mainLight.intensity;
+        spotLightMaxIntensity = 300f;
     }
 
     private void Update()
@@ -64,11 +78,26 @@ public class RoomWithItemsBehavior : MonoBehaviour
 
     void EnableUpgradeItems() //executed once
     {
+        LeanTween.value(gameObject, mainLightDefaultIntensity, 0.1f, 1f).setEase(LeanTweenType.easeInOutSine).setOnUpdate(ChangeMainLightIntensity);
+        LeanTween.value(gameObject, 0, spotLightMaxIntensity, 1f).setEase(LeanTweenType.easeInOutSine).setOnUpdate(ChangeSpotLightIntensity);
+
         for (int i = 0; i < spawnedUpgradeItems.Count; i++)
         {
             spawnedUpgradeItems[i].SetActive(true);
         }
     }
+
+    void ChangeMainLightIntensity(float value)
+    {
+        mainLight.intensity = value;
+    }
+
+    void ChangeSpotLightIntensity(float value)
+    {
+        spotLights[0].intensity = value;
+        spotLights[1].intensity = value;
+    }
+
 
     public void PlayerTookAnItem(GameObject takenItem)
     {
@@ -82,6 +111,19 @@ public class RoomWithItemsBehavior : MonoBehaviour
             LeanTween.moveLocalY(spawnedUpgradeItems[i], 65f, 1.25f).setEase(LeanTweenType.easeInCubic).setOnComplete(DestroySpawnedItemsInList);
             LeanTween.moveLocalZ(spawnedUpgradeItems[i], 25f, 0.35f).setEase(LeanTweenType.easeInSine).setDelay(0.9f);
         }
+        AnimateDoorOpen(doorTop);
+
+        LeanTween.value(gameObject, 0.1f, mainLightDefaultIntensity, 1f).setEase(LeanTweenType.easeInOutSine).setOnUpdate(ChangeMainLightIntensity);
+        LeanTween.value(gameObject, spotLightMaxIntensity, 0f, 1f).setEase(LeanTweenType.easeInOutSine).setOnUpdate(ChangeSpotLightIntensity);
+    }
+
+
+    private void AnimateDoorOpen(GameObject door)
+    {
+        LeanTween.move(door, door.transform.position - (Vector3.right * 2), 1f)
+            .setDelay(0.5f).setEase(LeanTweenType.easeInOutCubic);
+        LeanTween.scale(door, (doorDefaultScale - new Vector3(0.01f, 0, 0.01f)), 1f)
+            .setDelay(0.5f).setEase(LeanTweenType.easeInOutCubic);
     }
 
     private void OnTriggerEnter(Collider other)
