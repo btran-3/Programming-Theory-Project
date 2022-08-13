27566bb267f3b434bcb4065845bad058b0f9f48d
bt <britra832@gmail.com>
2022-08-12 20:13:01 -0400

    Filling main menu background with enemies

--- Assets/Scripts/MainMenuBackground.cs
+++ Assets/Scripts/MainMenuBackground.cs
@@ -10,17 +10,23 @@ public class MainMenuBackground : MonoBehaviour
     {
         LeanTween.rotateAround(gameObject, Vector3.up, -360, 16f).setLoopClamp();
         LeanTween.value(30f, 40f, 1.5f).setEaseInOutSine().setOnUpdate(SpotlightPulseBrightness).setLoopPingPong();
+        LeanTween.value(75f, 110f, 3f).setEaseInOutSine().setOnUpdate(SpotLightOuterAngle).setLoopPingPong();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     void SpotlightPulseBrightness(float value)
     {
         spotLight.intensity = value;
+    }
+    void SpotLightOuterAngle(float value)
+    {
+        spotLight.spotAngle = value;
+        Debug.Log(spotLight.spotAngle);
     }
 
     private void OnDestroy()
