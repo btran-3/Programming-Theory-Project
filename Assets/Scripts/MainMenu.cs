bbff3110a200ec3193ecd02d44eecaa13612f886
bt <britra832@gmail.com>
2022-08-14 08:46:44 -0400

    Main menu UI elements and layout

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField] GameObject mainMenu, optionsMenu;
+    [SerializeField] GameObject mainMenuFirstButton, optionsFirstButton, optionsClosedButton;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
