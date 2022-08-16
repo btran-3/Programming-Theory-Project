cd4aa9f229f4aab293fbe9f2405f4ce5e5a4e797
bt <britra832@gmail.com>
2022-08-16 00:05:22 -0400

    Main menu options text highlighting correctly

--- Assets/Scripts/Navigation/MainMenu.cs
+++ Assets/Scripts/Navigation/MainMenu.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using TMPro;
 using Rewired;
 
 public class MainMenu : MonoBehaviour
@@ -10,10 +12,17 @@ public class MainMenu : MonoBehaviour
     [SerializeField] GameObject mainMenuFirstButton, optionsFirstButton, optionsClosedButton;
 
     [SerializeField] GameObject newGameButton, continueButton, optionsButton, exitButton;
+    [SerializeField] GameObject optionsSoundEffectsSlider, optionsMusicSlider, optionsSoundEffectText, optionsMusicText;
+    GameObject lastSelected;
 
     private bool isMenuTransitioning;
     private float menuAnimationOffset = 250f;
     private float menuAnimationTime = 0.65f;
+
+    //private Color unselectedUIColor = new Color(0.196f, 0.196f, 0.196f, 1); //323232
+    //private Color selectedUIColor = new Color(0.96f, 0.96f, 0.96f, 1); //F5F5F5
+    private float unselectedUIColorFloat = 0.196f; //323232 divided by 255
+    private float selectedUIColorFloat = 0.96f; //F5F5F5 divided by 255
 
     //Rewired stuff
     private int playerId = 0;
@@ -51,6 +60,31 @@ public class MainMenu : MonoBehaviour
             CloseOptionsMenu();
             Debug.Log("successfully closing menu using Cancel Selection button");
         }
+
+        //Debug.Log(EventSystem.current);
+
+        if (EventSystem.current.currentSelectedGameObject == optionsSoundEffectsSlider && lastSelected != optionsSoundEffectsSlider)
+        {
+            lastSelected = optionsSoundEffectsSlider;
+            Debug.Log("options SFX slider has been selected");
+
+            LeanTween.cancel(optionsSoundEffectText);
+            LeanTween.cancel(optionsMusicText);
+            LeanTween.value(optionsSoundEffectText.GetComponent<TextMeshProUGUI>().color.r, selectedUIColorFloat, 0.1f).setOnUpdate(ColorSoundEffectsText);
+            LeanTween.value(optionsMusicText.GetComponent<TextMeshProUGUI>().color.r, unselectedUIColorFloat, 0.1f).setOnUpdate(ColorMusicText);
+        }
+        else if (EventSystem.current.currentSelectedGameObject == optionsMusicSlider && lastSelected != optionsMusicSlider)
+        {
+            lastSelected = optionsMusicSlider;
+            Debug.Log("options music slider has been selected");
+
+            LeanTween.cancel(optionsSoundEffectText);
+            LeanTween.cancel(optionsMusicText);
+            LeanTween.cancel(optionsSoundEffectText);
+            LeanTween.cancel(optionsMusicText);
+            LeanTween.value(optionsSoundEffectText.GetComponent<TextMeshProUGUI>().color.r, unselectedUIColorFloat, 0.1f).setOnUpdate(ColorSoundEffectsText);
+            LeanTween.value(optionsMusicText.GetComponent<TextMeshProUGUI>().color.r, selectedUIColorFloat, 0.1f).setOnUpdate(ColorMusicText);
+        }
     }
 
     public void OpenOptionsMenu()
@@ -72,6 +106,8 @@ public class MainMenu : MonoBehaviour
 
         EventSystem.current.SetSelectedGameObject(null);
         EventSystem.current.SetSelectedGameObject(optionsFirstButton);
+
+
     }
 
     private void CloseOptionsMenu()
@@ -131,6 +167,15 @@ public class MainMenu : MonoBehaviour
     void FadeOutOptionsMenu(float value)
     {
         optionsMenu.GetComponent<CanvasGroup>().alpha = value;
+    }
+
+    void ColorSoundEffectsText(float value)
+    {
+        optionsSoundEffectText.GetComponent<TextMeshProUGUI>().color = new Color(value, value, value, 1);
+    }
+    void ColorMusicText(float value)
+    {
+        optionsMusicText.GetComponent<TextMeshProUGUI>().color = new Color(value, value, value, 1);
     }
 
 }
