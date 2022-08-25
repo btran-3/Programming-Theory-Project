317fc65e5acee7743196b1a736bca471ad73d0b4
bt <britra832@gmail.com>
2022-08-25 18:57:57 -0400

    Floor generator working but player projectile bug

--- Assets/Scripts/Rooms/FloorLayoutManager.cs
+++ Assets/Scripts/Rooms/FloorLayoutManager.cs
@@ -7,14 +7,16 @@ using Unity.AI.Navigation;
 public class FloorLayoutManager : MonoBehaviour
 {
     [SerializeField] private GameObject startingRoom;
-    [SerializeField] private GameObject endingRoom;
-    [SerializeField] private GameObject[] possibleHostileRooms;
-    [SerializeField] private GameObject[] possibleKindRooms;
+    [SerializeField] private GameObject bossRoom;
+    [SerializeField] private GameObject itemRoom;
+    [SerializeField] private GameObject dispenserRoom;
+    [SerializeField] private GameObject storeRoom;
+    [SerializeField] private List<GameObject> possibleHostileRoomsList;
     [SerializeField] private List<GameObject> spawnedRooms;
 
     [SerializeField] private NavMeshSurface surface;
 
-    private int roomsToSpawn = 9;
+    private int roomsToSpawn = 12;
     private float roomZSpacing = 11f;
     private Vector3 startingRoomSpawnPos = Vector3.zero;
 
@@ -36,12 +38,12 @@ public class FloorLayoutManager : MonoBehaviour
 
     private void Awake()
     {
-        //SetAllPoolRoomsInactive();
+        SetAllPoolRoomsInactive();
     }
 
     private void Start()
     {
-        //SpawnRooms(roomsToSpawn);
+        SpawnRooms(roomsToSpawn);
 
         surface.BuildNavMesh();
 
@@ -55,20 +57,20 @@ public class FloorLayoutManager : MonoBehaviour
     private void SetAllPoolRoomsInactive()
     {
         startingRoom.SetActive(false);
-        endingRoom.SetActive(false);
+        bossRoom.SetActive(false);
+        itemRoom.SetActive(false);
+        dispenserRoom.SetActive(false);
+        storeRoom.SetActive(false);
 
-        for (int i = 0; i < possibleHostileRooms.Length; i++)
+        for (int i = 0; i < possibleHostileRoomsList.Count; i++)
         {
-            possibleHostileRooms[i].SetActive(false);
-        }
-        for (int i = 0; i < possibleKindRooms.Length; i++)
-        {
-            possibleKindRooms[i].SetActive(false);
+            possibleHostileRoomsList[i].SetActive(false);
         }
     }
 
     private void SpawnRooms(int roomsToSpawn)
     {
+        Debug.Log("change this to be in thirds");
         int halfwayPoint = Mathf.CeilToInt(roomsToSpawn/2);
         //Debug.Log(halfwayPoint);
 
@@ -76,30 +78,48 @@ public class FloorLayoutManager : MonoBehaviour
         {
             if (i == 0) //starting room
             {
-                GameObject room = Instantiate(startingRoom, startingRoomSpawnPos, Quaternion.identity);
-                room.SetActive(true);
+                startingRoom.SetActive(true);
+                startingRoom.transform.position = Vector3.zero;
+                //GameObject room = Instantiate(startingRoom, startingRoomSpawnPos, Quaternion.identity);
+                //room.SetActive(true);
             }
             else if (i == halfwayPoint) //a "kind" room roughly halfway through
             {
                 float zSpawnDist = i * roomZSpacing;
-                GameObject room = Instantiate(possibleKindRooms[0],
+                itemRoom.SetActive(true);
+                itemRoom.transform.position = new Vector3(0, 0, zSpawnDist);
+                /* GameObject room = Instantiate(itemRoom,
                     startingRoomSpawnPos + new Vector3(0, 0, zSpawnDist), Quaternion.identity);
-                room.SetActive(true);
+                room.SetActive(true); */
             }
-            else if (i == roomsToSpawn) //THIS IS SUPPOSED TO BE THE ENDING ROOM
+            else if (i == roomsToSpawn - 2) //store room right before boss
             {
                 float zSpawnDist = i * roomZSpacing;
-                GameObject room = Instantiate(endingRoom,
+                storeRoom.SetActive(true);
+                storeRoom.transform.position = new Vector3(0, 0, zSpawnDist);
+            }
+            else if (i == roomsToSpawn - 1) //final room (aka boss)
+            {
+                float zSpawnDist = i * roomZSpacing;
+                bossRoom.SetActive(true);
+                bossRoom.transform.position = new Vector3(0, 0, zSpawnDist);
+                /* GameObject room = Instantiate(endingRoom,
                     startingRoomSpawnPos + new Vector3(0, 0, zSpawnDist), Quaternion.identity);
-                room.SetActive(true);
+                room.SetActive(true); */
             }
             else //HOSTILE ROOM POOL
             {
                 float zSpawnDist = i * roomZSpacing;
-                int randRoomIndex = Random.Range(0, possibleHostileRooms.Length);
-                GameObject room = Instantiate(possibleHostileRooms[randRoomIndex],
+                int randRoomIndex = Random.Range(0, possibleHostileRoomsList.Count);
+                possibleHostileRoomsList[randRoomIndex].SetActive(true);
+                possibleHostileRoomsList[randRoomIndex].transform.position = new Vector3(0, 0, zSpawnDist);
+                possibleHostileRoomsList.RemoveAt(randRoomIndex);
+
+                /*
+                GameObject room = Instantiate(possibleHostileRoomsList[randRoomIndex],
                     startingRoomSpawnPos + new Vector3(0, 0, zSpawnDist), Quaternion.identity);
                 room.SetActive(true);
+                */
             }
         }
     }
